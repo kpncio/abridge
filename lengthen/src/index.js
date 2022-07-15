906977d7d5669ea7d6d171be51437180821ad613
albie6544 <49698766+albie6544@users.noreply.github.com>
2022-07-14 22:32:53 -0400

    Working as intended

---
+++ lengthen/src/index.js
@@ -0,0 +1,30 @@
+// Expects: Five Character Code:
+// kpnc.io/12345 --> https://calculator.aws
+
+async function handleRequest(request) {
+	try {
+		var short = /.+?\:\/\/.+?(\/.+?)(?:#|\?|$)/.exec(request.url)[1].replace(/[^\w\s]/gi, '');
+	} catch {
+		return Response.redirect('https://www.kpnc.io/', 301);
+	}
+
+	if (short.length != 5) {
+		return Response.redirect('https://www.kpnc.io/' + short, 302);
+	}
+
+	try {
+		var long = await kv.get(short);
+		long = long.split('|');
+		if (long[0] === null) {
+			return Response.redirect('https://www.kpnc.io/', 307);
+		} else {
+			return Response.redirect(long[0], 308);
+		}
+	} catch {
+		return Response.redirect('https://www.kpnc.io/', 307);
+	}
+}
+
+addEventListener('fetch', event => {
+	event.respondWith(handleRequest(event.request))
+})
---
+++ shorten/src/index.js
@@ -0,0 +1,51 @@
+// Expects: Any URI-Encoded URL:
+// https://app.kpnc.io/shorten/https%3A%2F%2Fcalculator.aws --> kpnc.io/12345
+
+async function handleRequest(request) {
+	function validurl(string) {
+		var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (res !== null)
+	}
+
+	try {
+		var long = decodeURIComponent(/.+?\:\/\/.+?(\/.+?)(?:#|\?|$)/.exec(request.url)[1].replace('/', '')).replace('shorten/', '');
+
+		if (long === 'wp-admin/' || long === 'favicon.ico') {
+			return new Response('Bad request...', {
+				headers: { 'content-type': 'text/plain', 'status' : 400 },
+			})
+		}
+	} catch {
+		return new Response('No long...', {
+			headers: { 'content-type': 'text/plain', 'status' : 400 },
+		})
+	}
+
+  if (!validurl(long)) {
+    return new Response('No valid long...', {
+      headers: { 'content-type': 'text/plain', 'status' : 400 },
+    })
+  }
+
+  var ip = request.headers.get('cf-connecting-ip');
+
+	while(true) {
+		var short = Math.random().toString(36).substr(2, 5).split('').map(c => Math.random() < 0.5 ? c.toUpperCase() : c).join('');
+
+		const check = await kv.get(short);
+
+		if (check === null) {
+			await kv.put(short, long + '|' + ip);
+
+			break;
+		}
+	}
+
+	return new Response('Success: ' + short + '\n' + 'Logged: ' + ip, {
+		headers: { 'content-type': 'text/plain', 'status' : 200 },
+	})
+}
+
+addEventListener('fetch', event => {
+	event.respondWith(handleRequest(event.request))
+})
