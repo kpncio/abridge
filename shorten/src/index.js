84f9277a1bef2729590f7e8427b0deb5565e9298
Albie <49698766+albie6544@users.noreply.github.com>
2022-07-20 02:09:24 -0400

    Start Interference Mitigation

--- shorten/src/index.js
+++ shorten/src/index.js
@@ -10,24 +10,33 @@ async function handleRequest(request) {
 	try {
 		var long = decodeURIComponent(new URL(request.url).pathname.substring(1)).replace('shorten/', '');
 
-		if (long === 'wp-admin/' || long === 'favicon.ico') {
-			return new Response('Bad request...', {
-				headers: { 'content-type': 'text/plain', 'status' : 400 },
-			})
-		}
+		// const blocked = [ 'Google Cloud', 'Linode LLC', 'DigitalOcean LLC' ];
+		// blocked.forEach(organization => {
+		// 	if (request.cf.asOrganization == organization) {
+		// 		return new Response('Bad request...', {
+		// 			headers: { 'content-type': 'text/plain', 'status' : 400 },
+		// 		})
+		// 	}
+		// });
+
+		// if (long === 'wp-admin/' || long === 'favicon.ico') {
+		// 	return new Response('Bad request...', {
+		// 		headers: { 'content-type': 'text/plain', 'status' : 400 },
+		// 	})
+		// }
 	} catch {
 		return new Response('No long...', {
 			headers: { 'content-type': 'text/plain', 'status' : 400 },
 		})
 	}
 
-  if (!validurl(long)) {
-    return new Response('No valid long...', {
-      headers: { 'content-type': 'text/plain', 'status' : 400 },
-    })
-  }
+	if (!validurl(long)) {
+		return new Response('No valid long...', {
+		headers: { 'content-type': 'text/plain', 'status' : 400 },
+		})
+	}
 
-  var ip = request.headers.get('cf-connecting-ip');
+	var organization = request.cf.asOrganization;
 
 	while(true) {
 		var short = Math.random().toString(36).substr(2, 5).split('').map(c => Math.random() < 0.5 ? c.toUpperCase() : c).join('');
@@ -35,7 +44,7 @@ async function handleRequest(request) {
 		const check = await kv.get(short);
 
 		if (check === null) {
-			await kv.put(short, long + '|' + ip);
+			await kv.put(short, long + '|' + organization);
 
 			break;
 		}
